bc9de119b546ce75bd4a3ef482fe96ef0450d1c1
Marcus Pointner <marcus.pointner@gmail.com>
2014-11-13 07:41:55 +0200

    created an OpenFL proj from FD template

---
+++ xml_based_custom_button/src/com/example/Main.hx
@@ -0,0 +1,21 @@
+package com.example;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author pm
+ */
+
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+}
